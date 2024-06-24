4c7513499ed4a34a8ce5b1e1223f2fc1c8986adf
Inworker <31877506+Inworker@users.noreply.github.com>
2024-06-24 22:47:51 +0300

    Написал тесты для логина пользователя

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -1,7 +1,21 @@
 import allure
+import requests
+import urls
 class TestLoginUser:
 
-    @allure.description('')
-    def test_login_existing_user
-    @allure.description('')
-    def test_login_with_invalid_credentials
+    @allure.description('Логин под существующим пользователем')
+    def test_login_existing_user(self, data_new):
+        #Создал пользователя
+        responce_create_user = requests.post(urls.BASE_URL + urls.AUTH_REGISTER_USER_URL, data=data_new)
+        #Удалил имя пользователя из data_new
+        del data_new["name"]
+        #Сделал запрос авторизации
+        responce_login_user = requests.post(urls.BASE_URL + urls.AUTH_LOGIN_URL, data = data_new)
+        assert 200 == responce_login_user.status_code and responce_login_user.json()["user"]["email"] == data_new["email"]
+        token = responce_create_user.json()["accessToken"]
+        requests.delete(urls.BASE_URL + urls.AUTH_USER_URL, data=data_new, headers={"Autorization": token})
+
+    @allure.description('Логин с неверным логином и паролем')
+    def test_login_with_invalid_credentials(self):
+        responce_login_user = requests.post(urls.BASE_URL + urls.AUTH_LOGIN_URL)
+        assert 401 == responce_login_user.status_code and responce_login_user.json()["message"] == "email or password are incorrect"
