ea8d417818401f3fca6688bae9ed9191793468b3
Inworker <31877506+Inworker@users.noreply.github.com>
2024-06-25 22:51:27 +0300

    Добавил параметризацию для изменения парамаметров пользователя с авторизацией и без

--- tests/test_change_data_user.py
+++ tests/test_change_data_user.py
@@ -1,12 +1,19 @@
 import allure
+import pytest
 import requests
 import urls
 import conftest
 import helper
 class TestChangeData:
 
+    @pytest.mark.parametrize(
+        'field',
+        [
+            "email", "name"
+        ]
+    )
     @allure.description('Изменение данных пользователя с авторизицией')
-    def test_authenticated_access(self, data_new):
+    def test_authenticated_access(self, data_new, field):
         # Создал пользователя
         responce_create_user = requests.post(urls.BASE_URL + urls.AUTH_REGISTER_USER_URL, data=data_new)
         token = responce_create_user.json()["accessToken"]
@@ -14,17 +21,30 @@ class TestChangeData:
         del data_new["name"]
         #Сделал еще набор фейковых данных
         change_data_user = helper.CreateCurrierData.generate_fake_user_data()
+
+        # Выводим данные для отладки
             # Сделал запрос авторизации
         requests.post(urls.BASE_URL + urls.AUTH_LOGIN_URL, data=data_new)
+        # Выводим данные из ответа для отладки
+        response_data_user = requests.patch(urls.BASE_URL + urls.AUTH_USER_URL, headers = {"Authorization": token}, data= {field: change_data_user[field]})
 
-        response_data_user = requests.patch(urls.BASE_URL + urls.AUTH_USER_URL, headers = {"Authorization": token}, data= change_data_user)
-
-        assert 200 == response_data_user.status_code and response_data_user.json()["user"]["email"] == change_data_user["email"]
+        assert 200 == response_data_user.status_code and response_data_user.json()["user"][field] == change_data_user[field]
         requests.delete(urls.BASE_URL + urls.AUTH_USER_URL, data=data_new, headers={"Authorization": token})
 
+    @pytest.mark.parametrize(
+        'field',
+        [
+            "email", "name"
+        ]
+    )
     @allure.description('Изменение данных пользователя без авторизиции')
-    def test_unauthenticated_access(self, data_new):
-        response_data_user = requests.patch(urls.BASE_URL + urls.AUTH_USER_URL,  data= data_new)
+    def test_unauthenticated_access(self, data_new, field):
+        responce_create_user = requests.post(urls.BASE_URL + urls.AUTH_REGISTER_USER_URL, data=data_new)
+        # Сделал еще набор фейковых данных
+        change_data_user = helper.CreateCurrierData.generate_fake_user_data()
+        response_data_user = requests.patch(urls.BASE_URL + urls.AUTH_USER_URL,
+                                            data={field: change_data_user[field]})
+
         assert 401 == response_data_user.status_code and response_data_user.json()["message"] == "You should be authorised"
 
 
