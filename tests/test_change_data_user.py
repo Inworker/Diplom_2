403ce04e22f538cee300fda20915a5046d4344ae
Inworker <31877506+Inworker@users.noreply.github.com>
2024-06-25 22:00:12 +0300

    Написал тест для изменения данных не авторизованного пользователя

--- tests/test_change_data_user.py
+++ tests/test_change_data_user.py
@@ -1,6 +1,7 @@
 import allure
 import requests
 import urls
+import conftest
 import helper
 class TestChangeData:
 
@@ -11,14 +12,20 @@ class TestChangeData:
         token = responce_create_user.json()["accessToken"]
         # Удалил имя пользователя из data_new
         del data_new["name"]
-            # Сделал запрос авторизации
+        #Сделал еще набор фейковых данных
         change_data_user = helper.CreateCurrierData.generate_fake_user_data()
+            # Сделал запрос авторизации
         requests.post(urls.BASE_URL + urls.AUTH_LOGIN_URL, data=data_new)
 
-        responce_data_user = requests.patch(urls.BASE_URL + urls.AUTH_USER_URL, headers = {"Authorization": token}, data= change_data_user)
+        response_data_user = requests.patch(urls.BASE_URL + urls.AUTH_USER_URL, headers = {"Authorization": token}, data= change_data_user)
 
-        assert 200 == responce_data_user.status_code and responce_data_user.json()["user"]["email"] == change_data_user["email"]
+        assert 200 == response_data_user.status_code and response_data_user.json()["user"]["email"] == change_data_user["email"]
         requests.delete(urls.BASE_URL + urls.AUTH_USER_URL, data=data_new, headers={"Authorization": token})
 
-    # @allure.description('Изменение данных пользователя без авторизиции')
-    # def test_unauthenticated_access(self):
+    @allure.description('Изменение данных пользователя без авторизиции')
+    def test_unauthenticated_access(self, data_new):
+        response_data_user = requests.patch(urls.BASE_URL + urls.AUTH_USER_URL,  data= data_new)
+        assert 401 == response_data_user.status_code and response_data_user.json()["message"] == "You should be authorised"
+
+
+
